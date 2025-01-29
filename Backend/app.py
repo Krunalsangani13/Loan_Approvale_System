b43bee6e8e9a188778398313ecb5e47817a5af31
Krunalsangani13 <khsangani3344@gmail.com>
2025-01-30 01:02:56 +0530

    Update app.py
    adding features like income not zero or loan amount not less then total assets(income, luxury, commercial, residential assets)

--- Backend/app.py
+++ Backend/app.py
@@ -15,29 +15,45 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data=request.json
+    data=request.get_json()
     try:
-        features=np.array([[
-            data['no_of_dependents'],
-            data['education'],
-            data['self_employed'],
-            data['income_annum'],
-            data['loan_amount'],
-            data['loan_term'],
-            data['cibil_score'],
-            data['residential_assets_value'],
-            data['commercial_assets_value'],
-            data['luxury_assets_value'],
-            data['bank_asset_value']
-        ]])
+       
+        features=np.array([
+        data.get("no_of_dependents",0),
+        data.get("education",0),
+        data.get("self_employed",0),
+        data.get("income_annum",0),
+        data.get("loan_amount",0),
+        data.get("loan_term",0),
+        data.get("cibil_score",0),
+        data.get("residential_assets_value",0),
+        data.get("commercial_assets_value",0),
+        data.get("luxury_assets_value",0),
+        data.get("bank_asset_value",0)
+        ],dtype=float).reshape(1,-1)
+
+        if data["income_annum"]==0:
+            return jsonify({"loan_status":"Rejected due to Zero income"}),400
+
+        total_assets=(
+            data.get("residential_assets_value",0)+
+            data.get("commercial_assets_value",0)+
+            data.get("bank_asset_value",0)+
+            data.get("luxury_assets_value",0)+
+            data.get("income_annum",0)
+        )
+
+        if data["loan_amount"]>total_assets:
+            return jsonify({"loan_status":"Rejected due to insufficient collateral"})
+        
 
         features_scaled=scaler.transform(features)
-        prediction=model.predict(features_scaled)[0]
-        result="Approved" if prediction == 1 else "Rejected"
+        prediction=model.predict(features_scaled)
+        loan_status="Approved" if prediction[0]==1 else "Rejected"
         
-        return jsonify({'loan_status':result})
+        return jsonify({'loan_status':loan_status})
     except Exception as e:
-        return jsonify({"error":str(e)})
+        return jsonify({"error":str(e)}),500
 
 if __name__ == '__main__':
     app.run(debug=True)
